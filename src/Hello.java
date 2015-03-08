fbecf841725f64ea06ba0c8d214337166996729e
Shiva Sharma <shivathebravo@gmail.com>
2015-03-08 12:02:53 -0400

    Java code Hello world

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		Print();
+	}
+
+	public static void Print() {
+		int a=10;
+		a=a+5;
+		int j=a;
+		System.out.println("Hello World");                                    
+		System.out.print("Hello World2");
+	}
+
+}
