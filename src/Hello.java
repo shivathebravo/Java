70ba44b88b18afaaf9ec54a062c9682c2c10c5ef
Shiva Sharma <shivathebravo@gmail.com>
2015-06-03 21:38:06 -0400

    new file

--- src/Hello.java
+++ src/Hello.java
@@ -1,17 +1,20 @@
 public class Hello {
 
+	private static String name;
+
 	public static void main(String[] args) {
 		
 		Print();
 	}
 
 	public static void Print() {
-		int a=10;
-		a=a+5;
-		int j=a;
+		
 		System.out.println("Hello World");                                    
 		System.out.print("Hello World2");
-		System.out.print("hello world3");
+		 System.out.println("Passed Name is :" + name ); 
+		 
+		  Puppy myPuppy = new Puppy( "tommy" );
+		
 	}
 
 }
